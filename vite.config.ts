0c4ea589cae26029e5608c53a8055278b1b5cf59
Raiwe17 <eliseymorozov20@gmail.com>
2026-02-18 20:24:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Добавьте эту строку. Важно: слэши в начале и в конце обязательны!
+      base: '/Project_9.0/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
